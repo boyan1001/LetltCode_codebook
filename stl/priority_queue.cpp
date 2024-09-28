c1b94157ef5fcc37957254324013829baee996ee
boyan <cby931001@gmail.com>
2024-09-28 21:02:23 +0800

    Add heap

--- stl/priority_queue.cpp
+++ stl/priority_queue.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main(){
     IOS;
-    priority_queue<int> pq;
+    priority_queue<int> pq; // max heap
 
     // push(): Insert element into the priority queue
     pq.push(3);
@@ -29,5 +29,15 @@ int main(){
 
     // empty(): Check if the priority queue is empty
     cout << pq.empty() << endl; // 0
+
+    priority_queue<int, vector<int>, greater<int>> pq2; // min heap
+
+    pq2.push(3);
+    pq2.push(5);
+    pq2.push(1);
+    pq2.push(2);
+    pq2.push(4);
+
+    cout << pq2.top() << endl; // 1
     return 0;
 }
